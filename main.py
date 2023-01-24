56dd8d41fb3f76576c04c53eabb40e499a04b4a2
Ruslan91111 <ruslan_ru911@mail.ru>
2023-01-25 00:07:55 +0500

    second not push

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def something(a, b):
-    return a + b
+def something(a, b, c):
+    return (a + b) * c
 
 
 a = 10
 b = 20
+c = 3
 
 if __name__ == '__main__':
-    print(something(a, b))
+    print(something(a, b, c))
